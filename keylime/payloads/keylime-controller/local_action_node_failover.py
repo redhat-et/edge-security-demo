aba9a813728e45aa914c84929513dc33c6ba9148
Michael Peters <mpeters@redhat.com>
2020-07-30 16:08:46 -0400

    Keylime revocation scripts to remove a worker node from OC

--- keylime/payloads/keylime-controller/local_action_node_failover.py
+++ keylime/payloads/keylime-controller/local_action_node_failover.py
@@ -1,27 +1,23 @@
 import os
 import asyncio
+import subprocess
+
+"""Node fail over script
+
+This script will be sent to the keylime_agent running on a machine
+which has an OpenShift client (oc) installed and configured.
+
+When a node is compromised this script will run `oc adm cordon` and
+`oc adm drain` commands to remove that worker node from the OpenShift
+cluster
+"""
 
 async def execute(revocation):
-    """Node fail over script
-
-    This script will be sent to the keylime_agent running on a machine
-    which has an OpenShift client (oc) installed and configured.
-
-    When a node is compromised this script will run `oc adm cordon` and
-    `oc adm drain` commands to remove that worker node from the OpenShift
-    cluster
-    """
     if revocation['type']!='revocation':
         return
 
-    # TODO - get the node name
-    node_name = "XXX"
-    rc = os.system("oc adm cordon {}".format(node_name))
-    if rc != 0:
-        print("Could not cordon node {}: Command failed with code {}".format(node_name, rc))
-        return
-
-    rc = os.system("oc adm drain {}".format(node_name))
-    if rc != 0:
-        print("Could not drain node {}: Command failed with code {}".format(node_name, rc))
-        return
+    node_name = 'mpeters-okd-zxrh5-' + revocation['agent_id']
+    cmd = "KUBECONFIG=/etc/kubeconfig /usr/local/bin/oc adm cordon {}".format(node_name)
+    subprocess.call(cmd, shell=True)
+    cmd = "KUBECONFIG=/etc/kubeconfig /usr/local/bin/oc adm drain {} --force=true --ignore-daemonsets --delete-local-data".format(node_name)
+    subprocess.call(cmd, shell=True)
